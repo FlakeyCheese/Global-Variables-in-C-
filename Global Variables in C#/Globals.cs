e2efb52dfe6846e92145db42055b1468a8b5964c
FlakeyCheese <rgblackett@gmail.com>
2024-01-17 10:05:55 +0000

    Add project files.

---
+++ Global Variables in C#/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Global_Variables_in_C_
+{
+    internal static class Globals
+    {
+        public static string stringValue;
+    }
+}
